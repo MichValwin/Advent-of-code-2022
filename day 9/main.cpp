c3855c13eb1381d90d251ccc1cd0f5a381b8985d
MichaelValwin <macm241192@gmail.com>
2022-12-09 16:26:07 +0100

    Use u64 for indexing and a better function to go to Head

--- day 9/main.cpp
+++ day 9/main.cpp
@@ -1,30 +1,39 @@
 #include <fstream>
 #include <iostream>
 #include <string>
-#include <vector>
-#include <stack>
 #include <cmath>
 #include <unordered_set>
 
 
-static const char* INPUT_FILE = "bigboy.txt";
+#define BIGBOY
 
-struct Position{
+#ifdef BIGBOY
+	/*
+		BIGBOY
+		SILVER: 8129855
+		GOLD: 7750850
+	*/
+    static const char* INPUT_FILE = "bigboy.txt";
+#else
+    static const char* INPUT_FILE = "input.txt";
+#endif
+
+struct Position {
 	int32_t x;
 	int32_t y;
 };
 
-std::string toStringPos(Position &p) {
-	return std::to_string(p.x) + "," + std::to_string(p.y);
+uint64_t toU64ForIndexing(const Position &p) {
+	return ((uint64_t)p.x << 32) + (uint64_t)p.y;
 }
 
-void doPhysics(Position &heads, Position &tails) {
+void doPhysics(const Position &heads, Position &tails) {
 	// Calculate distance
 	int32_t totalDistanceX = std::abs(heads.x - tails.x);
 	int32_t totalDistanceY = std::abs(heads.y - tails.y);
-
+	
+	/*
 	int32_t totalDistance = totalDistanceX + totalDistanceY;
-
 	if(totalDistanceX > 1 || totalDistanceY > 1){
 		if(totalDistance > 2) {
 			//Move diagonally
@@ -56,10 +65,27 @@ void doPhysics(Position &heads, Position &tails) {
 			}
 		}
 	}
+	*/
+
+	
+	if(totalDistanceX > 1 || totalDistanceY > 1) {
+		if(heads.x > tails.x) {
+			tails.x++;
+		}else if(heads.x < tails.x) {
+			tails.x--;
+		}
+
+		if(heads.y > tails.y) {
+			tails.y++;
+		}else if(heads.y < tails.y) {
+			tails.y--;
+		}
+	}
+	
 }
 
 void moveDirection(Position &pos, char direction) {
-	switch(direction){
+	switch(direction) {
 		case 'U':
 			pos.y++;
 		break;
@@ -84,42 +110,38 @@ int main() {
 		exit(0);
 	}
 
-	std::unordered_set<std::string> uniquePositionsSilver;
-	std::unordered_set<std::string> uniquePositionsGold;
-
-	Position heads = {0, 0};
-	Position tails = {0, 0};
-
+	std::unordered_set<uint64_t> uniquePositionsSilver;
+	std::unordered_set<uint64_t> uniquePositionsGold;
+	
 	Position longerRope[10] = {0, 0};
+	
+	uniquePositionsSilver.reserve(9000000);
+	uniquePositionsGold.reserve(9000000);
+	uniquePositionsSilver.insert(toU64ForIndexing(longerRope[1]));
+	uniquePositionsGold.insert(toU64ForIndexing(longerRope[9]));
+	
 
-	uniquePositionsSilver.insert(toStringPos(tails));
-	uniquePositionsGold.insert(toStringPos(longerRope[9]));
-
-    std::string line;
-    while(std::getline(inputFile, line)) {
+	std::string line;
+	while(std::getline(inputFile, line)) {
 		char direction = line[0];
 		int32_t numSteps = std::atoi(line.substr(2).c_str());
 
 		for(int32_t i = 0; i < numSteps; i++) {
-			// Move heads
-			moveDirection(heads, direction);
-			// Compute tail physics
-			doPhysics(heads, tails);
-			uniquePositionsSilver.insert(toStringPos(tails));
-
-			// Gold
-			//Move head??
+			// Move head to position indicated
+			// Compute physics in order 
 			moveDirection(longerRope[0], direction);
-			for(int i = 1; i < 10; i++) {
+			for(uint8_t i = 1; i < 10; i++) {
 				doPhysics(longerRope[i-1], longerRope[i]);
 			}
-
-			uniquePositionsGold.insert(toStringPos(longerRope[9]));
+			// Silver
+			uniquePositionsSilver.insert(toU64ForIndexing(longerRope[1]));
+			// Gold
+			uniquePositionsGold.insert(toU64ForIndexing(longerRope[9]));
 		}
-    }
+	}
 
 	std::cout << "Unique positions Silver: " << uniquePositionsSilver.size() << std::endl;
-	std::cout << "Unique positions Gold: " << uniquePositionsGold.size() << std::endl; 
+	std::cout << "Unique positions Gold: " << uniquePositionsGold.size() << std::endl;
 
 	return 0;
 }
